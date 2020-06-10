c48b5d7a2811b9c6c6d61a14b7dab4a5182c68f7
diegoslm <diegosdlm00@gmal.com>
2020-06-10 18:28:50 +0200

    cfd ensemble averaging y=0

--- 5. cfd_planes_of_interest+projection.py
+++ 5. cfd_planes_of_interest+projection.py
@@ -62,35 +62,38 @@ def z_0():
 def y_0():
     df_o = None
     df_i = None
-
+    y = 0
     for x in chain(range(-20, 20)):
         for z in range(-15, 15):
             files = []
-
-            y = 0
             if os.path.isfile(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z)):
-                df_o = pd.read_csv(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z), header=0)
-            y = -1
-            if os.path.isfile(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z)):
-                df_i = pd.read_csv(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z), header=0)
+                copyfile(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z),
+                         r'C:\Users\dsanc\Documents\GitHub\Data_Analysis\data\bins_cfd\y=0\[0, 1)\{}_{}_{}.csv'.format(
+                             x, y, z))
 
-            if not isinstance(df_o, type(None)):
-                files.append(df_o)
-            if not isinstance(df_i, type(None)):
-                files.append(df_i)
-
-            if not isinstance(df_o, type(None)) or not isinstance(df_i, type(None)):
-                df = pd.concat(files)
-            else:
-                df = None
-
-            if not isinstance(df, type(None)):
-                df.to_csv(
-                    r'C:\Users\dsanc\Documents\GitHub\Data_Analysis\data\bins_cfd\z=0\[0, 1)\{}_{}_{}.csv'.format(x, y,
-                                                                                                                  z))
-            print(len(files))
-            df_o = None
-            df_i = None
+            # y = 0
+            # if os.path.isfile(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z)):
+            #     df_o = pd.read_csv(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z), header=0)
+            # y = -1
+            # if os.path.isfile(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z)):
+            #     df_i = pd.read_csv(r'C:\Users\dsanc\Python\Python36\100bins\{}_{}_{}.csv'.format(x, y, z), header=0)
+            #
+            # if not isinstance(df_o, type(None)):
+            #     files.append(df_o)
+            # if not isinstance(df_i, type(None)):
+            #     files.append(df_i)
+            #
+            # if not isinstance(df_o, type(None)) or not isinstance(df_i, type(None)):
+            #     df = pd.concat(files)
+            # else:
+            #     df = None
+            #
+            # if not isinstance(df, type(None)):
+            #     df.to_csv(
+            #         r'C:\Users\dsanc\Documents\GitHub\Data_Analysis\data\bins_cfd\z=0\[0, 1)\{}_{}_{}.csv'.format(x, y,
+            #                                                                                                       z))
+            # df_o = None
+            # df_i = None
 
 
     for file in listdir(r'C:\Users\dsanc\Documents\GitHub\Data_Analysis\data\bins_cfd\y=0\[0, 1)'):
@@ -161,7 +164,7 @@ def x_10():
         df.to_csv(r'C:\Users\dsanc\Documents\GitHub\Data_Analysis\data\bins_cfd\x=10\x=10_projected\{}'.format(file))
 
 
-z_0()
+#z_0()
 y_0()
-x_10()
-x_m10()
+#x_10()
+#x_m10()
